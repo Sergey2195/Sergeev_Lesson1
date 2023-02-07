bac6ed67600747d27c539076c6faf06d5341a52a
serser <Sergey21.95@gmail.com>
2023-02-07 09:58:07 +0300

    add fun getDiceString

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main(){
-    printState(DiceState.ResultState(3))
+    printState(DiceState.ResultState(1))
 }
 
 fun printState(state: DiceState){
     val output = when (state){
         is DiceState.InitialState -> "Введите количество бросков кубика:"
         is DiceState.ThrowingState -> "Бросание кубика..."
-        is DiceState.ResultState -> "Бросок №${state.throwNumber}. Результат:${
+        is DiceState.ResultState -> "Бросок №${state.throwNumber}. Результат: ${
             getDiceString(state.getThrowingResult())}"
         is DiceState.FinishState -> "Конец"
     }
@@ -14,5 +14,13 @@ fun printState(state: DiceState){
 }
 
 fun getDiceString(dice: Int):String{
-    return "test"
+    return when (dice) {
+        1 -> "⚀"
+        2 -> "⚁"
+        3 -> "⚂"
+        4 -> "⚃"
+        5 -> "⚄"
+        6 -> "⚅"
+        else -> throw java.lang.RuntimeException("Error")
+    }
 }
